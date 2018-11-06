c07690deed6185182d76272694b9ebf3f7f8c44e
Blair Conrad <blair@blairconrad.com>
2018-11-06 12:27:43 -0500

    Use BenchmarkDotNet 0.11.2

--- src/FakeItEasy.Benchmark/Program.cs
+++ src/FakeItEasy.Benchmark/Program.cs
@@ -58,7 +58,7 @@
                     .WithLaunchCount(1)
                     // .WithWarmupCount(1)
                     .With(CsProjCoreToolchain.Current.Value)
-                    .WithNuget("FakeItEasy", versions[i]).WithId(versions[i]);
+                    .WithNuGet("FakeItEasy", versions[i]).WithId(versions[i]);
 
                     if (i == 0)
                     {
